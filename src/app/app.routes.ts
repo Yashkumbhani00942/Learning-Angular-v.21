1c9cdcad3145764f9ae5d49b38cd20974373bec2
ky patel <yashkumbhani1147@gmail.com>
2026-03-07 16:10:13 +0530

    move components in dashboard-main folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 import { Home } from './home/home';
 import { Form } from './form/form';
 import { Products } from './products/products';
@@ -6,6 +7,19 @@ import { About } from './about/about';
 import { Contact } from './contact/contact';
 import { Login } from './login/login';
 import { Userlist } from './userlist/userlist';
+=======
+import { Login } from './auth-components/login/login';
+import { Registration } from './auth-components/registration/registration';
+import { Dashboard } from './dashboard-main/dashboard/dashboard';
+import { Home } from './dashboard-main/home/home';
+import { Products } from './dashboard-main/products/products';
+import { About } from './dashboard-main/about/about';
+import { Contact } from './dashboard-main/contact/contact';
+import { Form } from './dashboard-main/form/form';
+import { Userlist } from './dashboard-main/userlist/userlist';
+import { PageNotFound } from './dashboard-main/page-not-found/page-not-found';
+
+>>>>>>> Stashed changes
 
 export const routes: Routes = [
     {
---
+++ src/app/dashboard-main/about/about.ts
@@ -0,0 +1,43 @@
+
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-about',
+  imports: [CommonModule,FormsModule ],
+  templateUrl: './about.html',
+  styleUrl: './about.scss',
+})
+export class About {
+
+  courseName: string ="Learning Angular version 21"
+
+  inputType: string ="message";
+  minlength: number = 7;
+  maxlength: number = 10;
+
+  constructor(){
+
+  }
+
+  showMessage(){
+    alert("Welcome to the about page")
+  }
+
+  OnCityChange(){
+    alert(`city changed ${{value: String}}`);
+  }
+
+  allData(val:string){
+    console.log(val);  
+  }
+
+
+  BusinessUnit = "GCC";
+  className="info";
+  inputcheck="checkbox";
+
+
+
+}
