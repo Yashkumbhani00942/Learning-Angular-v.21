579f8247d7c29e198a1e3e171ccad16eaf0c2340
Yash Kumbhani <yashkumbhani0@gmail.com>
2026-03-10 14:57:14 +0530

    signal practice on home page

--- src/app/dashboard-main/home/home.ts
+++ src/app/dashboard-main/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { Button } from '../../reusableComponents/button/button';
 
 @Component({
@@ -9,17 +9,21 @@ import { Button } from '../../reusableComponents/button/button';
 })
 export class Home {
 
+  // -- -- -- Writable Signal -- -- -- 
+
   data = 10;
   count = signal(0); // 
 
   constructor() {
     effect(() => {
-      console.log(`this is data`, this.data);
+      // console.log(`this is data`, this.data);
 
-      console.log(`this is count`, this.count());
+      // console.log(`this is count`, this.count());
       if (this.count() == 11) {
-        this.count.set(0)
+        this.count.set(0)   // -- -- .set method used to overwrite a signal's current value entirely with a new, provided value.
       }
+
+      console.log(`Area is : ` + this.area())
 
     })
   }
@@ -32,8 +36,31 @@ export class Home {
     this.count.set(this.count()+1);
   }
 
-  // Computed Signal
 
-  // height = signal();
+  // -- -- -- Computed Signal -- -- -- 
+
+  height = signal(100);
+  width = signal(25);
+  // area = this.height*this.width;  // -- -- without signal
+  area = computed(() => this.height()*this.width());  // -- -- Call Back function inside a computed
+
+  increaseHeight() {
+    // this.height += 10;
+    // this.area = this.height*this.width;
+
+    this.height.set(this.height()+10);
+  }
+
+  increaseWidth() {
+    // this.width += 100;
+    // this.area = this.height*this.width;
+
+    this.width.set(this.width()+100);
+
+  }
+
+  // measureArea() {
+  //     this.area();
+  // }
 
 }
